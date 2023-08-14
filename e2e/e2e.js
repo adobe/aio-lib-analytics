4774627d14189dbb9ca4667bb4d7e51fc7add096
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2023-08-14 23:08:12 +0800

    fix: eslint issues

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -31,24 +31,25 @@ test('sdk init test', async () => {
   expect(sdkClient.token).toBe(token)
 })
 
-test('test getCollections and getCollectionById', async () => {
+test('getCollections and getCollectionById', async () => {
   // check success response
   var res = await sdkClient.getCollections({ limit: 5, page: 0 })
   expect(res.status).toEqual(200)
   if (res.body.content && res.body.content.length > 0) {
     const element = res.body.content[0]
     res = await sdkClient.getCollectionById(element.rsid)
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getUsageLogs', async () => {
+test('getUsageLogs', async () => {
   // check success response
   var res = await sdkClient.getUsageLogs('2021-01-01T00:00:00-07', '2021-01-02T14:32:33-07')
   expect(res.status).toEqual(200)
 })
 
-test('test getMetrics and getMetricById', async () => {
+test('getMetrics and getMetricById', async () => {
   // check success response
   let res = await sdkClient.getMetrics(rsid, { limit: 5 })
   expect(res.status).toEqual(200)
@@ -57,11 +58,12 @@ test('test getMetrics and getMetricById', async () => {
     const element = res.body[0]
     const id = element.id.replace('metrics/', '')
     res = await sdkClient.getMetricById(id, rsid)
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getCalculatedMetrics and getCalculatedMetricById', async () => {
+test('getCalculatedMetrics and getCalculatedMetricById', async () => {
   // check success response
   let res = await sdkClient.getCalculatedMetrics()
   expect(res.status).toEqual(200)
@@ -70,11 +72,12 @@ test('test getCalculatedMetrics and getCalculatedMetricById', async () => {
     const element = res.body.content[0]
     const id = element.id.replace('metrics/', '')
     res = await sdkClient.getCalculatedMetricById(id)
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getDateRanges and getDateRangeById', async () => {
+test('getDateRanges and getDateRangeById', async () => {
   // check success response
   let res = await sdkClient.getDateRanges()
   expect(res.status).toEqual(200)
@@ -82,11 +85,12 @@ test('test getDateRanges and getDateRangeById', async () => {
   if (res.body.totalElements > 0) {
     const element = res.body.content[0]
     res = await sdkClient.getDateRangeById(element.id)
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getDimensions and getDimensionById', async () => {
+test('getDimensions and getDimensionById', async () => {
   // check success response
   let res = await sdkClient.getDimensions(rsid)
   expect(res.status).toEqual(200)
@@ -95,22 +99,24 @@ test('test getDimensions and getDimensionById', async () => {
     const element = res.body[0]
     const id = element.id.replace('variables/', '')
     res = await sdkClient.getDimensionById(id, rsid)
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getSegments and validateSegment', async () => {
+test('getSegments and validateSegment', async () => {
   // check success response
   let res = await sdkClient.getSegments()
   expect(res.status).toEqual(200)
   if (res.body.totalElements.length > 0) {
     const element = res.body.content[0]
     res = await sdkClient.validateSegment(element.rsid, {})
+    // eslint-disable-next-line jest/no-conditional-expect
     expect(res.status).toEqual(200)
   }
 })
 
-test('test getUsers and getCurrentUser', async () => {
+test('getUsers and getCurrentUser', async () => {
   // check success response
   let res = await sdkClient.getUsers({ limit: 5 })
   expect(res.status).toEqual(200)
